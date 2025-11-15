eb32e9ff482b08b09b8fe58eb0b64c234274f75b
Duy <vuduy050903@gmail.com>
2025-11-15 16:23:49 +0700

    update

--- src/configs/themeConfig.ts
+++ src/configs/themeConfig.ts
@@ -54,7 +54,7 @@ export type Config = {
 }
 
 const themeConfig: Config = {
-  templateName: 'Vuexy',
+  templateName: 'Quiz Online',
   homePageUrl: '/dashboards/teacher',
   settingsCookieName: 'vuexy-mui-next-demo-1',
   mode: 'system', // 'system', 'light', 'dark'
